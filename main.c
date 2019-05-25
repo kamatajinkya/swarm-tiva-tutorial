15aae93d499bfabd233ec015cb5fa4814aed077f
Ajinkya Kamat <kamat.ajinkya@gmail.com>
2019-05-25 11:52:33 -0400

    Implemented and tested delay function

--- main.c
+++ main.c
@@ -2,9 +2,30 @@
  * main.c
  */
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+
+#include "inc/hw_sysctl.h"
+#include "inc/hw_gpio.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
 
 int main(void)
 {
-    return 0;
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+
+    while(1)
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0xFF);
+        SysCtlDelay(1000000);
+
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x00);
+        SysCtlDelay(1000000);
+    }
 
 }
